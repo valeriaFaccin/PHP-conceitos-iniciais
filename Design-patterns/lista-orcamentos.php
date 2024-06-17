b0d48972d40c92a6f48c3c65ad2325ff0134395b
valeriaFaccin <valeriafaccin00@gmail.com>
2024-06-17 16:30:20 -0300

    adicionando iteração sobre lista de Orçamentos com Iterator

---
+++ Design-patterns/lista-orcamentos.php
@@ -0,0 +1,32 @@
+<?php
+use Alura\DesignPattern\ListaDeOrcamento;
+use Alura\DesignPattern\Orcamento;
+
+require_once 'vendor/autoload.php';
+
+$orcamento1 = new Orcamento();
+$orcamento1->qtdItens = 7;
+$orcamento1->aprovar();
+$orcamento1->valor = 500;
+
+$orcamento2 = new Orcamento();
+$orcamento2->qtdItens = 3;
+$orcamento2->aprovar();
+$orcamento2->finalizar();
+$orcamento2->valor = 200;
+
+$orcamento3 = new Orcamento();
+$orcamento3->qtdItens = 12;
+$orcamento3->reprovar();
+$orcamento3->valor = 1500;
+
+$listaOrcamentos = new ListaDeOrcamento();
+$listaOrcamentos->adicionaOrcamento($orcamento1);
+$listaOrcamentos->adicionaOrcamento($orcamento2);
+$listaOrcamentos->adicionaOrcamento($orcamento3);
+
+foreach($listaOrcamentos as $orcamentos){
+    echo "Valor: " . $orcamentos->valor . PHP_EOL;
+    echo "Estado: " . get_class($orcamentos->estadoAtual) . PHP_EOL;
+    echo "Quantidade Itens" . $orcamentos->qtdItens .PHP_EOL;
+}
---
+++ Design-patterns/src/ListaDeOrcamento.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\EstadosOrcamento\Finalizado;
+
+class ListaDeOrcamento implements \IteratorAggregate
+{
+    private array $listaOrcamentos;
+
+    public function __construct()
+    {
+        $this->listaOrcamentos = [];
+    }
+
+    public function adicionaOrcamento(Orcamento $orcamento)
+    {
+        $this->listaOrcamentos[] = $orcamento;
+    }
+
+    public function getIterator() : \Traversable
+    {
+        return new \ArrayIterator($this->listaOrcamentos);
+    }
+
+    public function orcamentosFinalizados(): array
+{
+    return array_filter(
+        $this->listaOrcamentos,
+        fn (Orcamento $orcamento) => $orcamento->estadoAtual instanceof Finalizado
+    );
+}
+}
