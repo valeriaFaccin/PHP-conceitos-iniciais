d4fd39cccc81b5f08daadf40e88fdff6ba3f4af2
valeriaFaccin <valeriafaccin00@gmail.com>
2024-06-17 17:49:39 -0300

    criando um Adapter

---
+++ Design-patterns/src/RegistroDeOrcamento.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\EstadosOrcamento\Finalizado;
+use Alura\DesignPattern\http\HttpAdapter;
+
+class RegistroDeOrcamento
+{
+    private HttpAdapter $http;
+    public function __construct(HttpAdapter $http)
+    {
+        $this->http = $http;
+    }
+    public function registrar(Orcamento $orcamento) : void
+    {
+        if(!$orcamento->estadoAtual instanceof Finalizado){
+            throw new \DomainException('Apenas Orçamentos Finalizados podem ser registrados');
+        }
+
+        $this->http->post('http://api.registrar.orcamento', [
+            "valor" => $orcamento->valor,
+            "quantidade" => $orcamento->qtdItens
+        ]);
+    }
+}
---
+++ Design-patterns/src/http/HttpAdapter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alura\DesignPattern\http;
+
+class HttpAdapter
+{
+    public function post(string $url, array $data = []) : void {}
+}
