341d8fdd846645c761f8228f97530b45ac82b63d
valeriaFaccin <valeriafaccin00@gmail.com>
2024-06-25 14:56:49 -0300

    compondo orçamentos com Composite

--- Design-patterns/itens-orcamento.php
+++ Design-patterns/itens-orcamento.php
@@ -10,24 +10,24 @@ $item1->valor = 300;
 $item2 = new ItensOrcamento();
 $item2->valor = 500;
 
-//$orcamento->addItem($item1);
-//$orcamento->addItem($item2);
+$orcamento->addItens($item1);
+$orcamento->addItens($item2);
 
 $orcamentoAntigo = new Orcamento();
 $item3 = new ItensOrcamento();
 $item3->valor = 150;
-//$orcamentoAntigo->addItem($item3);
+$orcamentoAntigo->addItens($item3);
 
 $orcamentoMaisAntigoAinda = new Orcamento();
 $item4 = new ItensOrcamento();
 $item4->valor = 50;
 $item5 = new ItensOrcamento();
 $item5->valor = 100;
-//$orcamentoMaisAntigoAinda->addItem($item4);
-//$orcamentoMaisAntigoAinda->addItem($item5);
+$orcamentoMaisAntigoAinda->addItens($item4);
+$orcamentoMaisAntigoAinda->addItens($item5);
 
-//$orcamentoAntigo->addItem($orcamentoMaisAntigoAinda);
+$orcamentoAntigo->addItens($orcamentoMaisAntigoAinda);
 
-//$orcamento->addItem($orcamentoAntigo);
+$orcamento->addItens($orcamentoAntigo);
 
-echo $orcamento->valor();
+echo $orcamento->valor() . PHP_EOL;
